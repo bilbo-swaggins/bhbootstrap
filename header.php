004a73c170e86462e9100a1117626ae82bc3f4ee
Eric Willis <ewillis182@gmail.com>
2015-01-06 13:13:52 -0800

    Favicon

--- header.php
+++ header.php
@@ -12,6 +12,8 @@
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+  		<!-- Favicon -->
+  		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
 			<!-- Google Fonts -->
 			<!-- Raleway -->
